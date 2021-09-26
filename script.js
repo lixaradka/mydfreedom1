cfae207a4bdd86a891ce927ea3534d346afa1d6e
nikon <85578630+lixaradka@users.noreply.github.com>
2021-09-26 12:21:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+// let a;
+// a = 2;
+
+// let b = 0;
+// b=3;
+
+// console.log(a);
+
+// // const num = 2;
+// // num = 5;
+// // const.log(num);
+
+// var c = 1;
+// c = 45;
+// console.log(c);  
+
+// let num1= 34;
+// let num2 = 3;
+// console.log(num1 + num2);
+// console.log(num1 -num2);
+// console.log(num1 ** num2);
+// console.log(num1 % num2);
+
+// let name = 'NeNikon';
+// let profession = 'Nikon';
+// console.log(name + profession);
+// let age = 30;
+// console.log(name + age);
+// console.log('имя: ' + name + ',профессия: ' + profession);
+
+//let user = prompt('Ваше имя');
+
+//console.log('Привет,' + user);
+//alert('Я всплывающее окно');
+
+//alert('Привет,' + user);
+
+//confirm('вы уверены,что хотите уйти');
+
+//let userInputdata = prompt("Введите ваш возраст");
+//let numberUserInputdata = Number(userInputdata);
+//alert(numberUserInputdata + 10);
+
+//let bool = confirm('вы уверены,что хотите уйти');
+//console.log(bool);
+
+//console.log(5 > 3);
+
+//onsole.log(4 == '4');
+
+// if (10 < 6) {
+//     alert("Правда!");
+// } else {
+//     alert("Неправда!")
+// }
+
+// for (let i = 0; i<3; i++) {
+// alert("привет");
+// }
+
+//                        Задание 1
+
+
+// let user = prompt('Введите ваше имя');
+// alert('Привет,' + user);
+
+//                        Задание 2                      
+// let number = prompt('Введите ваше число');
+// let s = prompt('Введите вашу степень');
+// console.log(number ** s);
+
+//                        Задание 3
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите первое число'));
+// if (num1 > num2) {
+//     console.log('Большее число: ' + num1);
+// } else if (num2 > num1) {
+//     console.log( 'Большее число: ' + num2);
+// }
+// else {
+//     console.log('Числа равны')
+// }
+
+
+//                        Задание 4
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите первое число'));
+// if (num1 % num2 == 0) {
+//     console.log("Является");
+// } else {
+//     console.log("Не является")
+// }
+
+//                        Задание 6
+for (let i = 0; i<5; i++) {
+    let num = prompt("Введите число"); 
+}
+if (num == 10) {
+console.log('Равно 10');
+} else {
+console.log('не равно 10')
+}
+
+
+
+
+
